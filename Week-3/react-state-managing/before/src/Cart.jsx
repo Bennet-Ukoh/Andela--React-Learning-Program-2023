ddaffac4364aa23c1a664b63f600d0f584f72959
Bennet-Ukoh <bennetukoh@gmail.com>
2023-08-21 06:45:30 +0100

    form validation added

--- Week-3/react-state-managing/before/src/Cart.jsx
+++ Week-3/react-state-managing/before/src/Cart.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import useFetchAll from "./services/useFetchAll";
 import Spinner from "./Spinner";
+import { useNavigate } from "react-router-dom";
 
 export default function Cart({ cart, updateQuantity }) {
+  const navigate = useNavigate();
   const urls = cart.map((i) => `products/${i.id}`);
   const { data: products, loading, error } = useFetchAll(urls);
 
@@ -49,11 +51,17 @@ export default function Cart({ cart, updateQuantity }) {
       <h1>
         {numItemsInCart === 0
           ? "Your cart is empty"
-          : `${numItemsInCart} Item${
-              numItemsInCart > 1 ? "s" : ""
-            } in my cart`}
+          : `${numItemsInCart} Item${numItemsInCart > 1 ? "s" : ""} in my cart`}
       </h1>
       <ul>{cart.map(renderItem)}</ul>
+      {cart.length > 0 && (
+        <button
+          onClick={() => navigate("/checkout")}
+          className="btn btn-primary"
+        >
+          Checkout
+        </button>
+      )}
     </section>
   );
 }
